d0984d68eebc8a9ceba48a54bb2c4073cae284ea
Rodrigo de Brito Maluf <rbrito@cit.local>
2018-07-24 13:13:34 -0300

    Criando service com objeto de menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  menus : string[];
+
+  constructor(private headerService : HeaderService) { 
+
+    this.menus = this.headerService.getMenus();
+  }
 
   ngOnInit() {
   }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { HeaderService } from './header.service';
 
 @NgModule({
   imports: [
@@ -9,6 +10,7 @@ import { HeaderComponent } from './header.component';
   exports: [
     HeaderComponent
   ],
-  declarations: [HeaderComponent]
+  declarations: [HeaderComponent],
+  providers : [HeaderService]
 })
 export class HeaderModule { }
