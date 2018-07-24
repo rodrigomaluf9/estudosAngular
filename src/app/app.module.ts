0f3c5c87ac25da5187347e3dfbd495de6ab094ef
Rodrigo de Brito Maluf <rbrito@cit.local>
2018-07-24 10:39:43 -0300

    criando o modulo header e sem componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HeaderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] 
 })
 export class AppModule { }
